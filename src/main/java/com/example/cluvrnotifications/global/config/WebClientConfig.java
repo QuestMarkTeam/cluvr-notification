3a865f721b50188cae7d462003f4f1ad400c2369
dnjs5024 <dnjs7469@naver.com>
2025-07-07 23:42:36 +0900

    Fix/#1 환경변수

--- src/main/java/com/example/cluvrnotifications/global/config/WebClientConfig.java
+++ src/main/java/com/example/cluvrnotifications/global/config/WebClientConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
 
 	@Value("https://api.cluvr.co.kr")
 	private String apiServerBaseUrl;
@@ -15,6 +17,20 @@ public class WebClientConfig {
 	public WebClient webClient() {
 		return WebClient.builder()
 			.baseUrl(apiServerBaseUrl)
+			.filter((request, next) -> {
+				// 요청 로그 추가
+				System.out.println("Request: " + request.method() + " " + request.url());
+				return next.exchange(request);
+			})
 			.build();
 	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("https://cluvr.co.kr")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("Content-Type", "Authorization", "X-Requested-With")
+			.allowCredentials(true);
+	}
+
 }
